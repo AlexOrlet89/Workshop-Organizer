a4bf4412c1c8fafc7e183d149d67b09a7f57870e
Alex Orlet <AlexOrlet89@gmail.com>
2022-02-21 17:54:03 -0800

    created addparticipant and workshop pages, connected buttons

--- workshops/workshops.js
+++ workshops/workshops.js
@@ -3,7 +3,13 @@ import { checkAuth, logout } from '../fetch-utils.js';
 checkAuth();
 
 const logoutButton = document.getElementById('logout');
+const addButton = document.getElementById('addParticipant');
 
 logoutButton.addEventListener('click', () => {
     logout();
+});
+
+addButton.addEventListener('click', ()=> {
+    console.log('click');
+    location.replace('../addParticipants');
 });

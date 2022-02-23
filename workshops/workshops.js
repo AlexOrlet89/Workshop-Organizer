88462189ce4cbcfbec0e924dbd4b6d2a584d2ba7
Alex Orlet <AlexOrlet89@gmail.com>
2022-02-22 16:31:18 -0800

    page load renders workshops

--- workshops/workshops.js
+++ workshops/workshops.js
@@ -2,9 +2,9 @@ import { checkAuth, logout, getWorkshops } from '../fetch-utils.js';
 import { renderWorkshop } from '../render-utils.js';
 
 checkAuth();
-
 const logoutButton = document.getElementById('logout');
 const addButton = document.getElementById('addParticipant');
+const catalog = document.getElementById('catalog');
 
 logoutButton.addEventListener('click', () => {
     logout();
@@ -13,4 +13,13 @@ logoutButton.addEventListener('click', () => {
 addButton.addEventListener('click', ()=> {
     console.log('click');
     location.replace('../addParticipants');
+});
+
+window.addEventListener('load', async () => {
+    const workshops = await getWorkshops();
+    console.log('window load', workshops);
+    for (let workshop of workshops) {
+        const div = await renderWorkshop(workshop);
+        catalog.append(div);
+    }
 });
